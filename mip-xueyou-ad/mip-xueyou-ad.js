b27aba02980ad3095b6bd3658a9d2353c3023c21
bjwangbin <myoa@163.com>
2017-09-11 21:31:16 +0800

    增加feed样式广告代码
    增加google adsense feed样式广告代码

--- mip-xueyou-ad/mip-xueyou-ad.js
+++ mip-xueyou-ad/mip-xueyou-ad.js
@@ -2,7 +2,9 @@
 * 学优网mip改造 第三方联盟广告插件
 * @file 插入联盟广告
 * @author myoa@163.com
-* @version 1.0.2
+* @time 2017.9.11
+* @version 1.0.3
+* @description 增加feed样式广告代码
 */
 define(function (require) {
     var $ = require('zepto');
@@ -12,11 +14,15 @@ define(function (require) {
         var el = this.element;
         var adtype = el.getAttribute('type');
         var token = el.getAttribute('token');
+        var elstyle = el.getAttribute('style');
+        var elformat = el.getAttribute('format');
+        var ellayout = el.getAttribute('layout');
+        var ellayoutkey = el.getAttribute('layout-key');
         var $element = $(el);
         var html = [];
         switch (adtype) {
             case 'alibaba':
-                if(!/micromessenger/.test(UA)){
+                if (!/micromessenger/.test(UA)) {
                     html.push('<a style="display:none!important" id="tanx-a-mm_10011797_2351313_' + token + '"></a>');
                     html.push('<script type="text/javascript">');
                     html.push('tanx_s = document.createElement("script");');
@@ -33,10 +39,20 @@ define(function (require) {
 
             case 'google' :
                 html.push('<ins class="adsbygoogle"');
-                html.push('     style="display:block"');
+                if (elstyle) {
+                    html.push('     style="' + elstyle + '"');
+                }
+                if (elformat) {
+                    html.push('     format="' + elformat + '"');
+                }
+                if (ellayout) {
+                    html.push('     layout="' + ellayout + '"');
+                }
+                if (ellayoutkey) {
+                    html.push('     layout-key="' + ellayoutkey + '"');
+                }
                 html.push('     data-ad-client="ca-pub-2743127670892157"');
                 html.push('     data-ad-slot="' + token + '"');
-                html.push('     data-ad-format="auto">');
                 html.push('</ins>');
                 html.push('<script>');
                 html.push('(adsbygoogle = window.adsbygoogle || []).push({});');
